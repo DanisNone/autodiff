d7df6ecbd48ece7ff2efb40323d15f57179c6f84
DanisNone <gubajdullindanis0@gmail.com>
2023-03-12 15:05:23 +0500

    .

--- autodiff/__main__.py
+++ autodiff/__main__.py
@@ -3,6 +3,7 @@ import autodiff as ad
 if __name__ == "__main__":
     x = ad.Variable("x")
     y = ad.Variable("y")
-    z = x + x - y + x - x
+    z = -x + y*5
     print(z)
-    print(z.optimize())
+    print(ad.optimize(z))
+    
